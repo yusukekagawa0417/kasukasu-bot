f79c827f904107b1fa8092b97b92feed60b68858
yusukekagawa0417 <y_kagawa0417@yahoo.co.jp>
2019-08-26 13:40:01 +0900

    Improve

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     // イベントオブジェクトを順次処理。
     req.body.events.forEach((event) => {
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
-        if (event.type == "message" && event.message.type == "text"){
+        // if (event.type == "message" && event.message.type == "text"){
             // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
             if (event.message.text == "こんにちは"){
                 // replyMessage()で返信し、そのプロミスをevents_processedに追加。
@@ -39,7 +39,7 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
                     text: "これはこれは"
                 }));
             }
-        }
+        // }
     });
 
     // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
