48d65d163402265c07c0950d65f8e57ba182cdac
yusukekagawa0417 <y_kagawa0417@yahoo.co.jp>
2019-08-27 00:09:29 +0900

    Improve

--- index.js
+++ index.js
@@ -51,14 +51,14 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
                     }
                 }).then((responses) => {
                     let message_text
-                    if (responses[0].queryResult.parameters.fields.gyagu.stringValue) {
-                        message_text = `${responses[0].queryResult.parameters.fields.gyagu.stringValue}`;
-                    }
                     if (responses[0].queryResult.parameters.fields.greeting.stringValue) { 
                         message_text = `よろしく`;
                     }
                     if (responses[0].queryResult.parameters.fields.question.stringValue) {
                         message_text = `それには答えられないな。何かやって欲しいギャグはあるかね？`;
+                    } 
+                    if (responses[0].queryResult.parameters.fields.gyagu.stringValue) {
+                        message_text = `${responses[0].queryResult.parameters.fields.gyagu.stringValue}`;
                     } else {
                         message_text = `でへへへへへ`;
                     }
