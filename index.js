1e19dd93fbd6437d75fdf35700b91ca75143f2bc
yusukekagawa0417 <y_kagawa0417@yahoo.co.jp>
2019-08-26 16:38:49 +0900

    Improve

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // モジュールのインポート
 const server = require("express")();
 const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
+const dialogflow = require("dialogflow");
 
 // -----------------------------------------------------------------------------
 // パラメータ設定
@@ -18,6 +19,15 @@ server.listen(process.env.PORT || 3000);
 // APIコールのためのクライアントインスタンスを作成
 const bot = new line.Client(line_config);
 
+// Dialogflowのクライアントインスタンスを作成
+const session_cliant = new dialogflow.SessionsClient({
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    credentials: {
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n")
+    }
+});
+
 // -----------------------------------------------------------------------------
 // ルーター設定
 server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
@@ -30,16 +40,37 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     // イベントオブジェクトを順次処理。
     req.body.events.forEach((event) => {
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
-        // if (event.type == "message" && event.message.type == "text"){
-            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-            if (event.message.text == "こんにちは"){
-                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-                events_processed.push(bot.replyMessage(event.replyToken, {
-                    type: "text",
-                    text: "これはこれは"
-                }));
-            }
-        // }
+        if (event.type == "message" && event.message.type == "text"){
+            events_processed.push(
+                session_client.detectIntent({
+                    session: session_client.sessionPath(process.env.GOOGLE_PROJECT_ID, event.source.userId),
+                    queryInput: {
+                        text: {
+                            text: event.message.text,
+                            languageCode: "ja"
+                        }
+                    }
+                }).then((responses) => {
+                    if (responses[0].queryResult &&  responses[0].queryResult.action == "greeting"){
+                        let message_text
+                        message_text = `トゥース！`
+                    }
+                    return bot.replyMessage(event.replyToken, {
+                        type: "text",
+                        text: message_text
+                    });
+                })
+            )
+            
+            // // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
+            // if (event.message.text == "こんにちは"){
+            //     // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+            //     events_processed.push(bot.replyMessage(event.replyToken, {
+            //         type: "text",
+            //         text: "これはこれは"
+            //     }));
+            // }
+        }
     });
 
     // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
