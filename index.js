c255270ad26f4c545f0838b46cd1e4c111154017
yusukekagawa0417 <y_kagawa0417@yahoo.co.jp>
2019-08-29 15:16:36 +0900

    Improve

--- index.js
+++ index.js
@@ -70,9 +70,9 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     });
 
     // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
-    Promise.all(events_processed).then(
-        (response) => {
-            console.log(`${response.length} event(s) processed.`);
-        }
-    );
+    // Promise.all(events_processed).then(
+    //     (response) => {
+    //         console.log(`${response.length} event(s) processed.`);
+    //     }
+    // );
 });
